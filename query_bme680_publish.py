7131bbc18531d3ef89417f460c5b8a5bc52b4883
AnHosu_Pi <andellegaard@gmail.com>
2020-01-13 15:16:44 +0100

    Added a method for publishing as a thing to Greengrass Core

---
+++ query_bme680_publish.py
@@ -0,0 +1,176 @@
+# /*
+# * Based on
+# * https://github.com/aws/aws-iot-device-sdk-python/blob/master/samples/greengrass/basicDiscovery.py
+# * and
+# * https://github.com/pimoroni/bme680-python/blob/master/examples/temperature-pressure-humidity.py
+# */
+
+
+import os
+import sys
+import time
+import uuid
+import json
+import logging
+import argparse
+from AWSIoTPythonSDK.core.greengrass.discovery.providers import DiscoveryInfoProvider
+from AWSIoTPythonSDK.core.protocol.connection.cores import ProgressiveBackOffCore
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.exception.AWSIoTExceptions import DiscoveryInvalidRequestException
+import bme680
+import smbus2
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY,smbus2.SMBus(1))
+except IOError:
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY,smbus2.SMBus(1))
+
+MAX_DISCOVERY_RETRIES = 10
+GROUP_CA_PATH = "./groupCA/"
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+clientId = "basic_env_sensor"
+thingName = "basic_env_sensor"
+topic = "bme/sensor/values"
+
+
+if not args.certificatePath or not args.privateKeyPath:
+    parser.error("Missing credentials for authentication, you must specify --cert and --key args.")
+    exit(2)
+
+if not os.path.isfile(rootCAPath):
+    parser.error("Root CA path does not exist {}".format(rootCAPath))
+    exit(3)
+
+if not os.path.isfile(certificatePath):
+    parser.error("No certificate found at {}".format(certificatePath))
+    exit(3)
+
+if not os.path.isfile(privateKeyPath):
+    parser.error("No private key found at {}".format(privateKeyPath))
+    exit(3)
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Progressive back off core
+backOffCore = ProgressiveBackOffCore()
+
+# Discover GGCs
+discoveryInfoProvider = DiscoveryInfoProvider()
+discoveryInfoProvider.configureEndpoint(host)
+discoveryInfoProvider.configureCredentials(rootCAPath, certificatePath, privateKeyPath)
+discoveryInfoProvider.configureTimeout(10)  # 10 sec
+
+retryCount = MAX_DISCOVERY_RETRIES
+discovered = False
+groupCA = None
+coreInfo = None
+while retryCount != 0:
+    try:
+        discoveryInfo = discoveryInfoProvider.discover(thingName)
+        caList = discoveryInfo.getAllCas()
+        coreList = discoveryInfo.getAllCores()
+
+        # We only pick the first ca and core info
+        groupId, ca = caList[0]
+        coreInfo = coreList[0]
+        print("Discovered GGC: %s from Group: %s" % (coreInfo.coreThingArn, groupId))
+
+        print("Now we persist the connectivity/identity information...")
+        groupCA = GROUP_CA_PATH + groupId + "_CA_" + str(uuid.uuid4()) + ".crt"
+        if not os.path.exists(GROUP_CA_PATH):
+            os.makedirs(GROUP_CA_PATH)
+        groupCAFile = open(groupCA, "w")
+        groupCAFile.write(ca)
+        groupCAFile.close()
+
+        discovered = True
+        print("Now proceed to the connecting flow...")
+        break
+    except DiscoveryInvalidRequestException as e:
+        print("Invalid discovery request detected!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        print("Stopping...")
+        break
+    except BaseException as e:
+        print("Error in discovery!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        retryCount -= 1
+        print("\n%d/%d retries left\n" % (retryCount, MAX_DISCOVERY_RETRIES))
+        print("Backing off...\n")
+        backOffCore.backOff()
+
+if not discovered:
+    print("Discovery failed after %d retries. Exiting...\n" % (MAX_DISCOVERY_RETRIES))
+    sys.exit(-1)
+
+# Iterate through all connection options for the core and use the first successful one
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureCredentials(groupCA, privateKeyPath, certificatePath)
+
+connected = False
+for connectivityInfo in coreInfo.connectivityInfoList:
+    currentHost = connectivityInfo.host
+    currentPort = connectivityInfo.port
+    print("Trying to connect to core at %s:%d" % (currentHost, currentPort))
+    myAWSIoTMQTTClient.configureEndpoint(currentHost, currentPort)
+    try:
+        myAWSIoTMQTTClient.connect()
+        connected = True
+        break
+    except BaseException as e:
+        print("Error in connect!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+
+if not connected:
+    print("Cannot connect to core %s. Exiting..." % coreInfo.coreThingArn)
+    sys.exit(-2)
+
+
+loopCount = 0
+while True:
+    message = {}
+    message['sequence'] = loopCount
+    if sensor.get_sensor_data():
+        message['temperature'] = sensor.data.temperature
+        message['pressure'] = sensor.data.pressure
+        message['humidity'] = sensor.data.humidity
+        message['message'] = "Succes"
+    else:
+        message['temperature'] = None
+        message['pressure'] = None
+        message['humidity'] = None
+        message['message'] = "Fail"
+    messageJson = json.dumps(message)
+    myAWSIoTMQTTClient.publish(topic, messageJson, 0)
+    print('Published topic %s: %s\n' % (topic, messageJson))
+    loopCount += 1
+    time.sleep(5)
