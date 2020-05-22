93e5c4c27d90100e207bbf0f075a5ab4efa93f15
AnHosu <andellegaard@gmail.com>
2020-05-22 12:46:56 +0200

    minor edits

--- gg_repub_shadow.py
+++ gg_repub_shadow.py
@@ -6,15 +6,37 @@ Created on Sat May 21 16:07:00 2020
 This is a simple example of a lambda function to be deployed
  to Greengrass core. It accompanies the demonstration at:
  https://github.com/AnHosu/iot_poc/blob/master/greengrass_ml.md
-The lambda function republishes values 
+The lambda function republishes values to the local Shadow service.
 """
 import greengrasssdk
 import logging
 import json
+import time
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+
+myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+myAWSIoTMQTTShadowClient.configureEndpoint(host, port)
+myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)
+myAWSIoTMQTTShadowClient.connect()
 
 REPUB_TOPIC = 'republish/reading'
 
 client = greengrasssdk.client('iot-data')
+
+# Gets the CPU temperature in degrees C
+def get_cpu_temperature():
+    try:
+        f = open("/sys/class/thermal/thermal_zone0/temp")
+        raw_temp = f.read()
+        f.close()
+        cpu_temp = float(raw_temp)/1000.0
+    except Exception as e:
+        logging.error('Unable to obtain CPU temperature. ' + repr(e))
+    return cpu_temp
 
 def get_topic(context):
     try:
@@ -22,12 +44,28 @@ def get_topic(context):
     except Exception as e:
         logging.error('Unable to read a topic. ' + repr(e))
     return topic
+    
+def get_temperature(event):
+    try:
+        temperature = float(event["temperature"])
+    except Exception as e:
+        logging.error('Unable to parse message body. ' + repr(e))
+    return temperature
 
 def function_handler(event, context):
     try:
         input_topic = get_topic(context)
+        input_temperature = get_temperature(event)
+        cpu_temps = []
+        for n in range(8):
+            cpu_temps.append(get_cpu_temperature())
+            time.sleep(1)
+        avg_cpu_temp = sum(cpu_temps)/float(len(cpu_temps))
+        # Compensated temperature
+        comp_temp = 2*input_temperature - avg_cpu_temp
         message = event
         message['input_topic'] = input_topic
+        message['temperature'] = comp_temp
         logging.info(event)
     except Exception as e:
         logging.error(e)
